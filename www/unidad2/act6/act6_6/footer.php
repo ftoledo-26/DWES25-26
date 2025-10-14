df9637bb6c02a3cb7eccd9c022a5227b522a75a7
ftoledo-26 <franciscomanueltoledo@gmail.com>
2025-10-14 22:56:56 +0200

    ejercicios php

--- www/unidad2/act6/act6_6/footer.php
+++ www/unidad2/act6/act6_6/footer.php
@@ -1,7 +1,7 @@
 <?php include 'config.php'; ?>
 <hr>
 <footer>
-    <p>&copy; <?php echo $anio; ?> - <?php echo $autor; ?></p>
+    <p>&copy; <?php echo $año; ?> - <?php echo $autor; ?></p>
 </footer>
 </body>
 </html>
