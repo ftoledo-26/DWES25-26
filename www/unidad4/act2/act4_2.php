ad4eacc74aade16c4afd29a15f1f9348acb4a4fa
Francisco Manuel Toledo Pérez <ftolper633@cpifpalanturing.es>
2025-11-10 20:32:31 +0100

    ejeercicios PDO

--- www/unidad4/act2/act4_2.php
+++ www/unidad4/act2/act4_2.php
@@ -1,26 +1,14 @@
 <?php
-/*
----
-title:  Ejercicio 1: Uso de sesiones para votaciones 
-desc: Implementa un sistema de votación simple utilizando sesiones para almacenar los votos. Muestra los resultados con barras de progreso.
-tags: [votación, sesiones, progreso]
-code: https://github.com/usuario/repo/blob/main/unidad1/ej2.php
----
-*/
-$host = 'mysql.miserver_db.alwaysdata.net';
-$db   = 'nombre_de_la_base';
-$user = 'usuario';
-$pass = 'contraseña';
-$charset = 'utf8mb4';
+$dsn = 'mysql:dbname=tienda;host=db';
+    $usuario = 'root';
+    $contraseña = 'root';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    try {
+        $mbd = new PDO($dsn, $usuario, $contraseña);
+        $mbd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Conexión exitosa a la base de datos.';
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-    echo "Conexión exitosa!";
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
-}
+    } catch (PDOException $e) {
+        echo 'Falló la conexión: ' . $e->getMessage();
+    }
 ?>
